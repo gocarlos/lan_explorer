45a3ceda3c7ab7eec3c09b15d022337ef06fb241
Carlos Gomes <kmartinho8@gmail.com>
2017-10-02 23:13:59 +0200

    added django stuff

--- src/gui.py
+++ src/gui.py
@@ -1,31 +1,99 @@
 from gi.repository import Gtk, Gdk, WebKit
+# gi.require_version('Gtk', '3.0')
 import sys
 import os
+import django
+import threading
+from threading import Thread
+from time import sleep
+import webview
+import threading
+import time
 
-class Browser(Gtk.Window):
-    def __init__(self, *args, **kwargs):
-        super(Browser, self).__init__(*args, **kwargs)
-        self.set_size_request(350,500)
-        self.connect("destroy", Gtk.main_quit)
 
-        self.webview = WebKit.WebView()
-        self._load_url()
+# t = webview
+#
+# class Browser(Gtk.Window):
+#     def __init__(self, *args, **kwargs):
+#         super(Browser, self).__init__(*args, **kwargs)
+#         self.set_size_request(350,500)
+#         self.connect("destroy", Gtk.main_quit)
+#
+#         self.webview = WebKit.WebView()
+#         self._load_url()
+#
+#         scrolled_window = Gtk.ScrolledWindow()
+#         scrolled_window.add(self.webview)
+#         self.add(scrolled_window)
+#
+#         scrolled_window.show_all()
+#         self.show()
+#
+#     def _load_url(self):
+#         # url = "file://" + sys.path[0] + "/res/index.html"
+#         url = "http://localhost:8000"
+#         self.webview.load_uri(url)
+#
+# def start_backend():
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lan_explorer.settings")
+#     from django.core.management import call_command
+#     from django.core.wsgi import get_wsgi_application
+#     application = get_wsgi_application()
+#     call_command('runserver',  '127.0.0.1:8000')
+#
+#
+# def start_frontend():
+#     webview.create_window("LAN Explorer", "http://127.0.0.1:8000/")
+#     # t.create_window("LAN Explorer")
 
-        scrolled_window = Gtk.ScrolledWindow()
-        scrolled_window.add(self.webview)
-        self.add(scrolled_window)
+class ThreadingExample(object):
+    """ Threading example class
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
 
-        scrolled_window.show_all()
-        self.show()
+    def __init__(self, interval=1):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.interval = interval
 
-    def _load_url(self):
-        # url = "file://" + sys.path[0] + "/res/index.html"
-        url = "http://localhost:8000"
-        self.webview.load_uri(url)
+        thread = threading.Thread(target=self.run, args=())
+        # thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+        thread2 = threading.Thread(target=self.run2, args=())
+        # thread.daemon = True                            # Daemonize thread
+        thread2.start()
+
+
+    def run(self):
+        """ Method that runs forever """
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lan_explorer.settings")
+        from django.core.management import call_command
+        from django.core.wsgi import get_wsgi_application
+        application = get_wsgi_application()
+        call_command('runserver',  '127.0.0.1:8000')
+
+    def run2(self):
+        """ Method that runs forever """
+        webview.create_window("LAN Explorer", "http://127.0.0.1:8000/")
 
 if __name__ == "__main__":
-    Gtk.init(sys.argv)
+    example = ThreadingExample()
 
-    browser = Browser()
 
-    Gtk.main()
+    # backend_thread = Thread(target = start_backend)
+    # # backend_thread.start()
+    # backend_thread.run()
+    # start_frontend()
+
+    # webview.load_url("http://localhost:8000")
+    # webview.create_window("LAN Explorer", "http://127.0.0.1:8000")
+
+    # Gtk.init(sys.argv)
+    #
+    # browser = Browser()
+    #
+    # Gtk.main()
--- src/lan_explorer.py
+++ src/lan_explorer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 import os
+import sys
 import time
 from subprocess import Popen, DEVNULL
 from lan_explorer_tools import Tools
 import subprocess
 import webview
+import django
+import threading
+from threading import Thread
+from time import sleep
+
 
 class LanExplorer():
 
@@ -33,12 +39,45 @@ class LanExplorer():
                         print('%s error' % ip)
                     break
 
+def start_backend():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lan_explorer.settings")
+    from django.core.management import call_command
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+    call_command('runserver',  '127.0.0.1:8000')
+
+# def start_frontend():
 
 if __name__ == '__main__':
     # Clear the screen
-    subprocess.call('clear', shell=True)
-    webview.create_window("It works, Jim!", "http://www.flowrl.com")
+    # subprocess.call('clear', shell=True)
 
-    tools = Tools()
-    l = LanExplorer(tools.get_my_ip())
-    l.get_ips()
+    Thread(target = start_backend).start()
+    sleep(1)
+    webview.create_window("LAN Explorer", "http://127.0.0.1:8000")
+    # Thread(target = start_frontend).start()
+
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lan_explorer.settings")
+    # try:
+    #     from django.core.management import execute_from_command_line
+    # except ImportError:
+    #     # The above import may fail for some other reason. Ensure that the
+    #     # issue is really that Django is missing to avoid masking other
+    #     # exceptions on Python 2.
+    #     try:
+    #         import django
+    #     except ImportError:
+    #         raise ImportError(
+    #     "Couldn't import Django. Are you sure it's installed and "
+    #     "available on your PYTHONPATH environment variable? Did you "
+    #     "forget to activate a virtual environment?"
+    #     )
+    #     raise
+    # if (len(sys.argv)>1):
+    #     execute_from_command_line(sys.argv)
+    # else:
+    #     execute_from_command_line("runserver")
+
+    # tools = Tools()
+    # l = LanExplorer(tools.get_my_ip())
+    # l.get_ips()
